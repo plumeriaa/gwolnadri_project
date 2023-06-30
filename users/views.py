59fff7d4939d9f7fcb7a2b307c5c683f2a195571
mkim <minkyungkim09@gmail.com>
2023-06-30 20:34:28 +0900

    Revert "[수정] 카카오로그인 수정 #5"
    This reverts commit b85c6b47b7fb129c7734cf18991696ec5a871336.

--- users/views.py
+++ users/views.py
@@ -251,26 +251,23 @@ class KakaoCallbackView(APIView):
 
 
         profile_json = profile_request.json()
-        kakao_account = profile_json["kakao_account"]
-        email = kakao_account["email"]
+        kakao_account = profile_json.get("kakao_account")
+        email = kakao_account.get("email")
 
         if email is None:
             return JsonResponse({'err_msg': '카카오 이메일을 가져오지 못했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # profile = kakao_account.get("profile")
-        # username = profile.get("nickname")
-        # profile_image = profile.get("thumbnail_image_url")   
-        username = email.split("@")[0]
-        print("이건유저네임", username)
+        profile = kakao_account.get("profile")
+        username = profile.get("nickname")
+        profile_image = profile.get("thumbnail_image_url")   
 
         try:
             user = User.objects.get(email=email)
             user.username = username
-            # user.profile_image = profile_image
+            user.profile_image = profile_image
             user.save()
             refresh = RefreshToken.for_user(user)
             refresh["email"] = user.email
-
             return Response(
                 {
                     "refresh": str(refresh),
@@ -281,11 +278,9 @@ class KakaoCallbackView(APIView):
 
         except User.DoesNotExist:
             password = User.objects.make_random_password()
-            username = email.split("@")[0]
             user = User.objects.create_user(email=email, password=password, username=username)
-        
             user.set_unusable_password()
-            # user.profile_image = profile_image
+            user.profile_image = profile_image
             user.save()
             refresh = RefreshToken.for_user(user)
             refresh["email"] = user.email
